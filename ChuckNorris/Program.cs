a88f98f5c7b3f97cff5bebdaf087af337a74148b
Artur <aleivate@tktk.ee>
2022-01-11 12:20:59 +0200

    Nädal 8

---
+++ ChuckNorris/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace ChuckNorris
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string randomPhrase;
+            randomPhrase = GetRandomFromFile("chuck.txt");
+            
+            Console.WriteLine(randomPhrase);
+
+        }
+
+        private static int GenerateRandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+            return randomIndex;
+        }
+
+        private static string GetRandomFromFile(string fileName)
+        {
+            string filePath = $@"C:\Users\kasutaja\...\samples\chuck.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+            string randomElement = dataFromFile[GenerateRandomIndex(dataFromFile)];
+
+            return randomElement;
+        }
+
+
+    }
+}
---
+++ FridayNightRandomReturn/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FridayNightRandomReturn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string randomFood, randomDrink, randomMovie;
+            string[] foods = { "chicken wings", "pizza", "sushi", "popcorn", "salat" };
+            string[] drinks = { "apple juice", "coke", "fanta", "sprite" };
+            string[] movies = { "Pulp Fiction", "The Godfather", "Shrek", "No Time To Die" };
+
+            randomFood = PickRandomElementFromArray(foods);
+            randomDrink = PickRandomElementFromArray(drinks);  
+            randomMovie = PickRandomElementFromArray(movies);
+
+            Console.WriteLine($"Tonight you will have some {randomFood} with {randomDrink} and watch {randomMovie}.");
+
+        }
+
+        private static int GenerateRandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+
+            return randomIndex;
+        }
+
+        private static string PickRandomElementFromArray(string[] someArray)
+        {
+            string randomElement = someArray[GenerateRandomIndex(someArray)];
+            return randomElement;
+
+        }
+
+    }
+}
